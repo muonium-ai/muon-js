9810bc3ad32fb6250f6254de15c80922f6b4b3e6
senthilnayagam <senthil@muonium.ai>
2026-01-31 10:09:50 +0530

    Compile basic arithmetic expressions to bytecode

--- src/compiler.rs
+++ src/compiler.rs
@@ -3,8 +3,6 @@
 
 use crate::bytecode::{BytecodeFunction, BytecodeModule, Instruction, OpCode};
 use crate::helpers::{is_simple_string_literal, number_to_value};
-use crate::types::JSValue;
-use crate::api::parse_numeric_expr;
 use crate::JSContextImpl;
 
 #[derive(Debug)]
@@ -20,12 +18,27 @@ impl Compiler {
     }
 
     pub fn compile_program(&mut self, ctx: &mut JSContextImpl, src: &str) -> Result<BytecodeModule, CompileError> {
-        let value = compile_literal(ctx, src)?;
-        let mut func = BytecodeFunction::new(None);
-        func.constants.push(value);
-        func.code.push(Instruction { op: OpCode::Const, a: 0, b: 0, c: 0 });
-        func.code.push(Instruction { op: OpCode::Return, a: 0, b: 0, c: 0 });
-        Ok(BytecodeModule::new(func))
+        let s = src.trim();
+        if is_simple_string_literal(s) {
+            let mut func = BytecodeFunction::new(None);
+            let inner = &s[1..s.len() - 1];
+            let value = crate::api::js_new_string(ctx, inner);
+            func.constants.push(value);
+            func.code.push(Instruction { op: OpCode::Const, a: 0, b: 0, c: 0 });
+            func.code.push(Instruction { op: OpCode::Return, a: 0, b: 0, c: 0 });
+            return Ok(BytecodeModule::new(func));
+        }
+
+        let mut expr = ExprCompiler::new(ctx, s);
+        expr.parse_expr()?;
+        expr.skip_ws();
+        if expr.pos != expr.input.len() {
+            return Err(CompileError {
+                message: "unsupported bytecode expression".to_string(),
+            });
+        }
+        expr.func.code.push(Instruction { op: OpCode::Return, a: 0, b: 0, c: 0 });
+        Ok(BytecodeModule::new(expr.func))
     }
 
     pub fn compile_empty(&mut self) -> BytecodeModule {
@@ -33,16 +46,143 @@ impl Compiler {
     }
 }
 
-fn compile_literal(ctx: &mut JSContextImpl, src: &str) -> Result<JSValue, CompileError> {
-    let s = src.trim();
-    if is_simple_string_literal(s) {
-        let inner = &s[1..s.len() - 1];
-        return Ok(crate::api::js_new_string(ctx, inner));
+struct ExprCompiler<'a> {
+    ctx: &'a mut JSContextImpl,
+    input: &'a [u8],
+    pos: usize,
+    func: BytecodeFunction,
+}
+
+impl<'a> ExprCompiler<'a> {
+    fn new(ctx: &'a mut JSContextImpl, src: &'a str) -> Self {
+        Self {
+            ctx,
+            input: src.as_bytes(),
+            pos: 0,
+            func: BytecodeFunction::new(None),
+        }
     }
-    if let Ok(num) = parse_numeric_expr(s) {
-        return Ok(number_to_value(ctx, num));
+
+    fn parse_expr(&mut self) -> Result<(), CompileError> {
+        self.parse_term()?;
+        loop {
+            self.skip_ws();
+            if self.consume(b'+') {
+                self.parse_term()?;
+                self.emit_op(OpCode::Add);
+            } else if self.consume(b'-') {
+                self.parse_term()?;
+                self.emit_op(OpCode::Sub);
+            } else {
+                break;
+            }
+        }
+        Ok(())
     }
-    Err(CompileError {
-        message: "unsupported bytecode literal".to_string(),
-    })
+
+    fn parse_term(&mut self) -> Result<(), CompileError> {
+        self.parse_factor()?;
+        loop {
+            self.skip_ws();
+            if self.consume(b'*') {
+                self.parse_factor()?;
+                self.emit_op(OpCode::Mul);
+            } else if self.consume(b'/') {
+                self.parse_factor()?;
+                self.emit_op(OpCode::Div);
+            } else {
+                break;
+            }
+        }
+        Ok(())
+    }
+
+    fn parse_factor(&mut self) -> Result<(), CompileError> {
+        self.skip_ws();
+        if self.consume(b'+') {
+            return self.parse_factor();
+        }
+        if self.consume(b'-') {
+            self.emit_const(0.0);
+            self.parse_factor()?;
+            self.emit_op(OpCode::Sub);
+            return Ok(());
+        }
+        if self.consume(b'(') {
+            self.parse_expr()?;
+            self.skip_ws();
+            if !self.consume(b')') {
+                return Err(CompileError { message: "missing ')'".to_string() });
+            }
+            return Ok(());
+        }
+        let num = self.parse_number()?;
+        self.emit_const(num);
+        Ok(())
+    }
+
+    fn parse_number(&mut self) -> Result<f64, CompileError> {
+        self.skip_ws();
+        let start = self.pos;
+        if self.peek() == Some(b'.') {
+            self.pos += 1;
+            self.consume_digits();
+        } else {
+            self.consume_digits();
+            if self.peek() == Some(b'.') {
+                self.pos += 1;
+                self.consume_digits();
+            }
+        }
+        if matches!(self.peek(), Some(b'e') | Some(b'E')) {
+            self.pos += 1;
+            if matches!(self.peek(), Some(b'+') | Some(b'-')) {
+                self.pos += 1;
+            }
+            self.consume_digits();
+        }
+        if self.pos == start {
+            return Err(CompileError { message: "expected number".to_string() });
+        }
+        let slice = &self.input[start..self.pos];
+        let s = core::str::from_utf8(slice).map_err(|_| CompileError { message: "invalid number".to_string() })?;
+        let n = s.parse::<f64>().map_err(|_| CompileError { message: "invalid number".to_string() })?;
+        Ok(n)
+    }
+
+    fn consume_digits(&mut self) {
+        while matches!(self.peek(), Some(b'0'..=b'9')) {
+            self.pos += 1;
+        }
+    }
+
+    fn emit_const(&mut self, num: f64) {
+        let value = number_to_value(self.ctx, num);
+        let idx = self.func.constants.len();
+        self.func.constants.push(value);
+        self.func.code.push(Instruction { op: OpCode::Const, a: idx as u32, b: 0, c: 0 });
+    }
+
+    fn emit_op(&mut self, op: OpCode) {
+        self.func.code.push(Instruction { op, a: 0, b: 0, c: 0 });
+    }
+
+    fn skip_ws(&mut self) {
+        while matches!(self.peek(), Some(b' ' | b'\n' | b'\r' | b'\t')) {
+            self.pos += 1;
+        }
+    }
+
+    fn consume(&mut self, b: u8) -> bool {
+        if self.peek() == Some(b) {
+            self.pos += 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn peek(&self) -> Option<u8> {
+        self.input.get(self.pos).copied()
+    }
 }
